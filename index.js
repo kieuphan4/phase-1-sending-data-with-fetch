d8f2fafbd4205b4e9fdc585a014647ee65189853
Kieu <kieuphan530@gmail.com>
2021-10-17 23:40:27 -0500

    commit tree 0670bcc0abbf799d6927ffb3bf384bc83f4fa1e4

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+const userObj = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+}
+
+function submitData() {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(userObj),
+    })
+    .then(resp => resp.json())
+    .then(object => document.body.innerHTML = object.id)
+    .catch(error => {
+        alert('Kowabummer, an error!');
+        document.body.innerHTML = error.message
+    })
+}
